1cd0f8e19ea155dd891f11e781aaff5e2ecdc6a8
Cassidy <cassidy.vance@hotmail.com>
2023-02-28 14:41:32 -0800

    added the demo file

---
+++ intro.js
@@ -0,0 +1,22 @@
+console.log(Hello, world!!!)
+
+const firstName = "Cassidy"
+
+node intrconsole.log(firstName)
+
+let age = 28
+
+const isCool = true
+
+console.log(firstName, age, isCool)
+
+
+let x = 4
+let y = 10
+let z = x + 8
+
+let sum = x + y +z
+
+// console.log(x)
+// console.log(y)
+// console.log(z)
